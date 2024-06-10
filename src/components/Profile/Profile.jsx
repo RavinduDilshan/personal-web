c91bc2b5a46e923d92af1e6cd6995abb47da2ba2
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-06-10 11:07:28 +0530

    cv download button added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { getImageUrl } from '../../utils'
 import styles from './Profile.module.css'
 import { SocialIcon } from 'react-social-icons'
+import { DownloadCvButton } from '../DownloadCvButton/DownloadCvButton'
 
 export const Profile = () => {
   return (
@@ -10,6 +11,7 @@ export const Profile = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi I'm Ravindu</h1>
             <p className={styles.description}>As a dynamic Full Stack Software Engineer with over 2 years of hands-on experience, I thrive on tackling complex challenges and transforming innovative ideas into robust, scalable solutions. My expertise spans both front-end and back-end development, allowing me to create seamless, high-performance applications. Driven by a passion for continuous learning and problem-solving, I am dedicated to delivering exceptional results and pushing the boundaries of technology. Let's build something extraordinary together.</p>
+            <DownloadCvButton />  
             <div className={styles.socialIcons}>
             <SocialIcon url="https://www.linkedin.com/in/ravidudilshan/" />
             <SocialIcon url="https://github.com/RavinduDilshan" />
@@ -20,7 +22,8 @@ export const Profile = () => {
           
             </div>
             
-          <p className={styles.downloadCvBtn}>this is download resume button</p> 
+             
+            
         </div>
        
     </section>
